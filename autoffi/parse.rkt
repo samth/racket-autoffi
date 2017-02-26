fb8a6bf043ce9d9d8388d1a9e3d24a47d7249e28
Sam Vervaeck <sam@vervaeck.be>
2017-02-26 22:13:24 +0100

    Fix parser to allow cycles

--- autoffi/parse.rkt
+++ autoffi/parse.rkt
@@ -10,6 +10,7 @@
          empty-catalog
          
          type
+         cyclic-record-type?
          type?
          primitive-type
          primitive-type?
@@ -20,6 +21,7 @@
          function-type-param-types
          record-type?
          record-type-fields
+         record-type-field-count
          struct-type
          struct-type?
          union-type
@@ -27,6 +29,7 @@
          pointer-type?
          enum-type
          enum-type?
+         enum-type-values
          pointer-type
          array-type?
          fixed-array-type
@@ -41,16 +44,84 @@
 
 (struct type ())
 (struct primitive-type type (id))
-(struct function-type type (result-type param-types));
+(struct function-type type ([result-type #:mutable] [param-types #:mutable]))
 (struct record-type type (fields))
 (struct struct-type record-type ())
+(struct enum-type type (values))
 (struct union-type record-type ())
-(struct array-type type (element-type))
+(struct array-type type ([element-type #:mutable]))
 (struct fixed-array-type array-type (element-count))
 (struct variadic-array-type array-type ())
-(struct pointer-type type (referenced-type))
+(struct pointer-type type ([referenced-type #:mutable]))
 (struct block-pointer-type pointer-type ())
-(struct enum-type type (values))
+
+;; Checks to see if a type contains itself
+;; without going into infinite recursion
+;(define (contains-self? tt)
+  ;(define visited (mutable-set))
+  ;(let has-self? ((tt tt))
+    ;(or (set-member? visited tt)
+        ;(begin 
+          ;(set-add! visited tt)
+          ;(cond
+            ;((or (enum-type? tt)
+                 ;(primitive-type? tt))
+             ;#f)
+            ;((function-type? tt)
+             ;(or (find-self (function-type-result-type tt))
+                 ;(ormap find-self (function-type-param-types tt))))
+            ;((record-type? tt)
+             ;(ormap find-self (hash-values (record-type-fields tt))))
+            ;((array-type? tt)
+             ;(find-self (array-type-element-type tt)))
+            ;((pointer-type? tt)
+             ;(find-self (pointer-type-referenced-type tt)))
+            ;(else (error 'cyclic-type? "unrecognised type ~a" tt)))))))
+
+
+;(define (cyclic-type? tt)
+  ;(define visited (mutable-set))
+  ;(let has-cycle? ((tt tt))
+    ;(and (not (set-member? visited))
+      ;(cond
+        ;((or (enum-type? tt)
+             ;(primitive-type? tt))
+         ;#f)
+        ;((function-type? tt)
+         ;(or (has-cycle? (function-type-result-type tt))
+             ;(ormap has-cycle? (function-type-param-types tt))))
+        ;((record-type? tt)
+         ;(define field-types (hash-values (record-type-fields tt)))
+         ;(or (ormap find-self field-types)
+             ;(ormap has-cycle? field-types)))
+
+        ;((array-type? tt)
+         ;(find-self (array-type-element-type tt)))
+        ;((pointer-type? tt)
+         ;(find-self (pointer-type-referenced-type tt)))
+        ;(else (error 'cyclic-type? "unrecognised type ~a" tt)))))
+
+(define (cyclic-record-type? rt)
+  (define (has-rt? tt)
+    (or (eq? tt rt)
+        (cond
+          ((function-type? tt)
+           (or  (has-rt? (function-type-result-type tt))
+                (ormap has-rt? (function-type-param-types tt))))
+          ((or (enum-type? tt)
+               (primitive-type? tt))
+           #f)
+          ((array-type? tt)
+           (has-rt? (array-type-element-type tt)))
+          ((pointer-type? tt)
+           (has-rt? (pointer-type-referenced-type tt))))))
+  (ormap has-rt? (hash-values (record-type-fields rt))))
+
+(define (record-type-field-count rt)
+  (hash-count (record-type-fields rt)))
+
+(define (add-record-field! rt field-name field-t)
+  (hash-set! (record-type-fields rt) field-name field-t ))
 
 (define (load-catalog name)
   (parse-catalog (read-json (open-input-file (string-append name ".json")))))
@@ -73,85 +144,122 @@
            #|(hash)|#
            #|type-list))|#
 
-  (define (type-folder type-spec dd)
+  (define ffi-types (mutable-ddict))
 
-    (define type-id (hash-ref type-spec 'id));
+  (let loop
 
-    (define (get-type-by-id id)
-      (ddict-ref dd id))
+    ((type-list type-list))
 
-    (define (get-fields type-spec)
-      (foldl (lambda (field-spec h)
-               (define name (hash-ref field-spec 'name))
-               (define type-id (hash-ref field-spec 'type))
-               (hash-set h name (get-type-by-id type-id)))
-             (hash)
-             (or (hash-get type-spec 'field) '())))
+    (when (not (null? type-list))
 
-    (define parsed-type
-     (case (hash-ref type-spec 'kind)
-       (("FUNCTION") 
-        (define result-type-id (hash-ref type-spec 'returnType))
-        (define params (map get-type-by-id (or (hash-get type-spec 'paramType) '())))
-        (function-type (get-type-by-id result-type-id) params))
-       (("POINTER")
-        (if (hash-get type-spec 'isBlockPointer)
-            (block-pointer-type (get-type-by-id (hash-ref type-spec 'referencedType)))
-            (pointer-type (get-type-by-id (hash-ref type-spec 'referencedType)))))
-       (("PRIMITIVE")
-        (define primitive-type-kind (hash-ref type-spec 'primitiveKind))
-        (primitive-type 
-          (case primitive-type-kind
-            (("Void") 'void)
-            (("Char_U" "Char_S") 'char)
-            (("SChar") 'schar)
-            (("UChar") 'uchar)
-            (("short") 'short)
-            (("SShort") 'sshort)
-            (("UShort") 'ushort)
-            (("Int") 'int)
-            (("Sint") 'sint)
-            (("UInt") 'uint)
-            (("long") 'long)
-            (("ULong") 'ulong)
-            (("SLong") 'slong)
-            (("LongLong") 'llong)
-            (("ULongLong") 'ullong)
-            (("SLongLong") 'sllong)
-            (("Float") 'float)
-            (("Double") 'double)
-            (("LongDouble") 'ldouble)
-            (("Bool") 'stdbool)
-            (else (error 'parse-catalog "could not determine the kind of primitive type for ~a: ~a" type-id primitive-type-kind)))))
-       (("STRUCT")
-        (struct-type (get-fields type-spec)))
-       (("UNION")
-        (union-type (get-fields type-spec)))
-       (("ENUM")
-        (enum-type (foldl (lambda (value-spec h)
-                                  (define name (hash-ref value-spec 'name))
-                                  (define value (hash-ref value-spec 'value))
-                                  (hash-set h name value))
-                                (hash)
-                                (or (hash-get type-spec 'value) '()))))
-       (("ARRAY")
-        (if (hash-has-key? type-spec 'count)
-            (fixed-array-type (get-type-by-id (hash-ref type-spec 'elementType))
-                                    (hash-ref type-spec 'count))
-            (variadic-array-type (get-type-by-id (hash-ref type-spec 'elementType)))))
-       (("QUALIFIED")
-        (get-type-by-id (hash-ref type-spec 'underlyingType))) ; drops qualifiers
-       (else (error 'parse-catalog "could not parse type spec for ~a: ~a" type-id (hash-ref type-spec 'kind)))))
-    (ddict-set dd type-id parsed-type))
-  (define type-ddict
-    (foldl type-folder (ddict) type-list))
+      (define type-spec (car type-list))
+
+      (define type-id (hash-ref type-spec 'id))
+
+      (define (get-type-by-id id)
+        (ddict-ref ffi-types id))
+
+      (define (add-fields rt type-spec)
+        (for-each (lambda (field-spec)
+                   (define name (hash-ref field-spec 'name))
+                   (define type-id (hash-ref field-spec 'type))
+                   (add-record-field! rt name (get-type-by-id type-id)))
+               (or (hash-get type-spec 'field) '())))
+
+      (define (get-enum-values type-spec)
+        (foldl (lambda (value-spec h)
+                 (define name (hash-ref value-spec 'name))
+                 (define value (or (hash-get value-spec 'value) 0))
+                 (hash-set h name value))
+               (hash)
+               (or (hash-get type-spec 'value) '())))
+
+      (case (hash-ref type-spec 'kind)
+        ; a function type's parameters and return value cannot circularly
+        ; reference the function type itself, so we're safe
+        (("FUNCTION") 
+         (define f (function-type #f '()))
+         (ddict-set! ffi-types type-id  f)
+         (loop (cdr type-list))
+         (set-function-type-result-type! f (get-type-by-id (hash-ref type-spec 'returnType)))
+         (set-function-type-param-types! f (map get-type-by-id (or (hash-get type-spec 'paramType) '()))))
+        (("POINTER")
+         (if (hash-get type-spec 'isBlockPointer)
+             (let ((b (block-pointer-type #f)))
+               (ddict-set! ffi-types type-id b)
+               (loop (cdr type-list))
+               (set-pointer-type-referenced-type! b (get-type-by-id (hash-ref type-spec 'referencedType))))
+             (let ((p (pointer-type #f)))
+               (ddict-set! ffi-types type-id p)
+               (loop (cdr type-list))
+               (set-pointer-type-referenced-type! p (get-type-by-id (hash-ref type-spec 'referencedType))))))
+        (("PRIMITIVE")
+         (define primitive-type-kind (hash-ref type-spec 'primitiveKind))
+         (ddict-set! ffi-types type-id
+           (primitive-type 
+             (case primitive-type-kind
+               (("Void") 'void)
+               (("Char_U" "Char_S") 'char)
+               (("SChar") 'schar)
+               (("UChar") 'uchar)
+               (("short") 'short)
+               (("SShort") 'sshort)
+               (("UShort") 'ushort)
+               (("Int") 'int)
+               (("Sint") 'sint)
+               (("UInt") 'uint)
+               (("long") 'long)
+               (("ULong") 'ulong)
+               (("SLong") 'slong)
+               (("LongLong") 'llong)
+               (("ULongLong") 'ullong)
+               (("SLongLong") 'sllong)
+               (("Float") 'float)
+               (("Double") 'double)
+               (("LongDouble") 'ldouble)
+               (("Bool") 'stdbool)
+               (else (error 'parse-catalog "could not determine the kind of primitive type for ~a: ~a" type-id primitive-type-kind)))))
+         (loop (cdr type-list)))
+        (("STRUCT")
+         (define s (struct-type (make-hash)))
+         (ddict-set! ffi-types type-id s)
+         (loop (cdr type-list))
+         (add-fields s type-spec))
+        (("UNION")
+         (define u (union-type (make-hash)))
+         (ddict-set! ffi-types type-id u)
+         (loop (cdr type-list)))
+        (("ENUM")
+         (ddict-set! ffi-types type-id
+           (enum-type (get-enum-values type-spec)))
+         (loop (cdr type-list)))
+        (("ARRAY")
+         (if (hash-has-key? type-spec 'count)
+             (let ((f (fixed-array-type #f)))
+               (ddict-set! ffi-types type-id f (hash-ref type-spec 'count))
+               (loop (cdr type-list))
+               (set-array-type-element-type! f (get-type-by-id (hash-ref type-spec 'elementType))))
+             (let ((v (variadic-array-type #f)))
+               (ddict-set! ffi-types type-id v)
+               (loop (cdr type-list))
+               (set-array-type-element-type! v (get-type-by-id (hash-ref type-spec 'elementType)))))
+         (loop (cdr type-list)))
+        ; qualified types in and by itself reference nothing
+        (("QUALIFIED")
+         (ddict-set! ffi-types type-id (get-type-by-id (hash-ref type-spec 'underlyingType)))
+         (loop (cdr type-list)))
+        (else (error 'parse-catalog "could not parse type spec for ~a: ~a" type-id (hash-ref type-spec 'kind))))))
+
   (define export-hash
     (foldl (lambda (export-spec h)
-             (define export-name (hash-ref export-spec 'name))
-             (define export-type-id (hash-ref export-spec 'type))
-             (hash-set h export-name (ddict-ref type-ddict export-type-id)))
+             ; FIXME: the compiler shouldn't add nameless exports at all
+             (if (hash-has-key? export-spec 'name)
+               (let ((export-name (hash-ref export-spec 'name))
+                     (export-type-id (hash-ref export-spec 'type)))
+                 (hash-set h export-name (ddict-ref ffi-types export-type-id)))
+               h))
            (hash)
            exports))
 
-  (catalog type-ddict export-hash))
+  (catalog ffi-types export-hash))
 
--- autoffi/syntax.rkt
+++ autoffi/syntax.rkt
@@ -1,45 +1,66 @@
 #lang racket
 
 (require ffi/unsafe
-         (for-syntax racket/syntax)
-         (for-syntax "parse.rkt"))
+         (for-syntax 
+           racket/syntax
+           syntax/srcloc
+           "parse.rkt"))
 
 (provide require/foreign
          (all-from-out ffi/unsafe))
 
-(define-for-syntax (tt-prim-kind->ffi-prim-kind kind-id)
-  (case kind-id
-     ((char) #'_int8)
-     ((schar) #'_sint8)
-     ((uchar) #'_uint8)
-     ((short) #'_int16)
-     ((sshort) #'_sint16)
-     ((ushort) #'_uint16)
-     ((int) #'_int32)
-     ((sint) #'_sint32)
-     ((uint) #'_uint32)
-     ((long) #'_long)
-     ((ulong) #'_ulong)
-     ((slong) #'_slong)
-     ((llong) #'_llong)
-     ((ullong) #'_ullong)
-     ((sllong) #'_sllong)
-     ((bool) #'_stdbool)
-     ((float) #'_float)
-     ((double) #'_double)
-     ((ldouble) #'_longdouble)
-     ((void) #'_void)
-     (else (error 'transit-type->ctype "could not convert primitive type: unrecorgnised id ~a" kind-id))))
+(begin-for-syntax
+
+  (define (tt-prim-kind->ffi-prim-kind kind-id)
+    (case kind-id
+       ((char) #'_int8)
+       ((schar) #'_sint8)
+       ((uchar) #'_uint8)
+       ((short) #'_int16)
+       ((sshort) #'_sint16)
+       ((ushort) #'_uint16)
+       ((int) #'_int32)
+       ((sint) #'_sint32)
+       ((uint) #'_uint32)
+       ((long) #'_long)
+       ((ulong) #'_ulong)
+       ((slong) #'_slong)
+       ((llong) #'_llong)
+       ((ullong) #'_ullong)
+       ((sllong) #'_sllong)
+       ((bool) #'_stdbool)
+       ((float) #'_float)
+       ((double) #'_double)
+       ((ldouble) #'_longdouble)
+       ((void) #'_void)
+       (else (error 'transit-type->ctype "could not convert primitive type: unrecorgnised id ~a" kind-id))))
+  
+  (define (dirname path) 
+    (let-values ([(base name must-be-dir?) (split-path path)])
+      base))
+
+  )
 
 (define-syntax (require/foreign stx)
   (syntax-case stx ()
     [(_ name)
      (string? (syntax-e #'name))
-     (let* ((catalog (load-catalog (syntax-e #'name)))
+     (let* ((catalog (load-catalog (string-append (path->string (dirname (source-location-source #'name))) (syntax-e #'name))))
             (export-hash (catalog-export-hash catalog))
             (types (catalog-types catalog)))
+
        (define function-export-names (filter (lambda (proc-name) (function-type? (hash-ref export-hash proc-name))) (hash-keys export-hash)))
+
        (define transformed (make-hash))
+
+       (define (get-enum-symbols h) 
+         (define keys (hash-keys h))
+         (let loop
+           ((symbols (hash-keys h)))
+           (if (null? symbols)
+              '()
+              (list* (string->symbol (car symbols)) '= (hash-ref h (car symbols)) (loop (cdr symbols))))))
+
        (define (transform-type tt)
          (define id (datum->syntax #'name (syntax-e (format-id #'name "_~a" (generate-temporary)))))
          (hash-set! transformed tt id)
@@ -50,34 +71,37 @@
             #`(define #,id (_cprocedure (list #,@(map (lambda (param-type) (hash-ref transformed param-type)) (function-type-param-types tt)))
                            #,(hash-ref transformed (function-type-result-type tt)))))
            ((struct-type? tt)
-            (define fields (record-type-fields tt))
-            (if (= (hash-count fields) 0)
-                #`(define #,id _pointer)
+            (if (and (not (cyclic-record-type? tt)) (> (record-type-field-count tt) 0))
+              (let ((fields (record-type-fields tt)))
                 #`(define-cstruct #,id
                           #,(map (lambda (field-name)
                                     (define field-type (hash-ref fields field-name))
                                     #`[#,(string->symbol field-name) #,(hash-ref transformed field-type)])
-                              (hash-keys fields)))));
+                              (hash-keys fields))))
+              #`(define #,id (_cpointer '#,id))))
            ((union-type? tt)
-            #`(define #,id _pointer))
+            #`(define #,id (_cpointer '#,id)))
            ((enum-type? tt)
-            #|#'(_enum #,(|#
-            'TODO)
+            #`(define #,id (_enum '#,(get-enum-symbols (enum-type-values tt)))))
            ((array-type? tt)
             #`(define #,id _pointer))
            ((pointer-type? tt)
             ; TODO: make typed pointers
             #`(define #,id _pointer))
            (else (error 'require/foreign "unknown autoffi type ~a" tt))))
-    (let ((type-defs (map transform-type types)))
-      (with-syntax ([proc-names (datum->syntax #'name (map string->symbol function-export-names))]
-                    [proc-defs 
-                      (datum->syntax #'name (map (lambda (proc-name)
-                                                   (define proc-type (hash-ref export-hash proc-name))
-                                                   #`(get-ffi-obj #,proc-name lib #,(hash-ref transformed proc-type)))
-                                function-export-names))])
-        #`(define-values proc-names
-          (let ((lib (ffi-lib name)))
-            #,@type-defs
-            (values . proc-defs))))))]))
+       
+       (define ffi-lib-name (generate-temporary))
+       (define type-defs (map transform-type types))
+       (define proc-defs 
+         (datum->syntax #'name (map (lambda (proc-name)
+                                      (define proc-type (hash-ref export-hash proc-name))
+                                      #`(get-ffi-obj #,proc-name #,ffi-lib-name #,(hash-ref transformed proc-type)))
+                                    function-export-names)))
+
+       (with-syntax ([proc-names (datum->syntax #'name (map string->symbol function-export-names))]
+                     [image-path (datum->syntax #'name (string-append (path->string (dirname (source-location-source #'name))) (syntax-e #'name)))])
+         #`(begin
+             (define #,ffi-lib-name (ffi-lib image-path))
+             #,@type-defs
+             (define-values proc-names (values #,@proc-defs)))))]))
 
